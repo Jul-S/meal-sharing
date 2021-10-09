ba6f99be80f5593b2a6c4333ac022b09baa63ebd
jul-s <jul.samorodova@gmail.com>
2021-10-09 19:06:29 +0200

    Fixed search meals

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -6,7 +6,7 @@ router.get("/", async (request, response) => {
   try {
 
     if (Object.keys(request.query).length !== 0) {
-      const suportedQuerry = ["maxPrice", "title", "availableReservations", "createdAfter", "limit"];
+      const suportedQuerry = ["maxPrice", "title", "availableReservations", "dateAfter", "limit"];
 
       for (const param of Object.keys(request.query)) {
         if (suportedQuerry.indexOf(param) === -1)
@@ -16,11 +16,11 @@ router.get("/", async (request, response) => {
       const maxPrice = request.query.maxPrice;
       const availableReservations = request.query.availableReservations;
       const title = request.query.title;
-      const createdAfter = request.query.createdAfter;
+      const dateAfter = request.query.dateAfter;
       const limit = request.query.limit;
 
       if (maxPrice && isNaN(parseInt(maxPrice))
-        || createdAfter && isNaN(Date.parse(createdAfter))
+        || dateAfter && isNaN(Date.parse(dateAfter))
         || limit && isNaN(parseInt(limit))
       ) {
         return response.send(400).json({ error: "Invalid query param" });
@@ -31,8 +31,8 @@ router.get("/", async (request, response) => {
 
       let filteredMeals = meals.filter(m =>
         (maxPrice ? (m.price < parseInt(maxPrice)) : true) &&
-        (title ? (m.title.includes(title)) : true) &&
-        (createdAfter ? (new Date(m.createdAt) > Date.parse(createdAfter)) : true)
+        (title ? (m.title.toUpperCase().includes(title.toUpperCase())) : true) &&
+        (dateAfter ? (new Date(m.when) > Date.parse(dateAfter)) : true)
       );
 
       filteredMeals = limit ? filteredMeals.slice(0, parseInt(limit)) : filteredMeals;
